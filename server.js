fb04a4a1b6d6fd9434756a9739d96004fbd0b4b3
David Ralph <ohlookitsderpy@protonmail.com>
2019-04-08 19:01:22 +0100

    Continue to refactor

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+const app              = require('connect')();
 const route            = require('connect-route');
-const connect          = require('connect');
 const connect_st       = require('st');
 const { readFileSync } = require('fs');
 
@@ -12,36 +12,26 @@ config.host = process.env.HOST || config.host || 'localhost';
 
 // build the store from the config on-demand - so that we don't load it
 // for statics
-if (!config.storage) config.storage = { type: 'file' };
-if (!config.storage.type) config.storage.type = 'file';
-
-let Store, preferredStore;
-Store = require('./lib/document_stores/' + config.storage.type);
-preferredStore = new Store(config.storage);
-
+let Store = require('./lib/document_store.js');
+let preferredStore = new Store(config.storage);
 
 // Send the static documents into the preferred store, skipping expirations
-let path, data;
 for (let name in config.documents) {
-  path = config.documents[name];
-  data = readFileSync(path, 'utf8');
+  let data = readFileSync(config.documents[name], 'utf8');
   console.log('Loading static document');
-  if (data) preferredStore.set(name, data, () => { console.log(`Loaded static document ${name}`); }, true);
+  if (data) preferredStore.set(name, data, () => console.log(`Loaded static document ${name}`), true);
   else console.log(`Failed to load static document ${name}`);
 }
 
 // Configure the document handler
-let documentHandler = new DocumentHandler({ store: preferredStore, maxLength: config.maxLength });
-
-let app = connect();
+let documentHandler = new DocumentHandler({ store: preferredStore });
 
 // first look at API calls
 app.use(route((router) => {
   // get raw documents - support getting with extension
   router.get('/raw/:id', (request, response) => {
     let key = request.params.id.split('.')[0];
-    let skipExpire = !!config.documents[key];
-    return documentHandler.handleRawGet(key, response, skipExpire);
+    return documentHandler.handleRawGet(key, response, !!config.documents[key]);
   });
   // add documents
   router.post('/documents', (request, response) => {
@@ -50,15 +40,13 @@ app.use(route((router) => {
   // get documents
   router.get('/documents/:id', (request, response) => {
     let key = request.params.id.split('.')[0];
-    let skipExpire = !!config.documents[key];
-    return documentHandler.handleGet(key, response, skipExpire);
+    return documentHandler.handleGet(key, response, !!config.documents[key]);
   });
 }));
 
 // Otherwise, try to match static files
 app.use(connect_st({
   path: __dirname + '/static',
-  content: { maxAge: config.staticMaxAge },
   passthrough: true,
   index: false
 }));
@@ -75,10 +63,7 @@ app.use(route((router) => {
 // And match index
 app.use(connect_st({
   path: __dirname + '/static',
-  content: { maxAge: config.staticMaxAge },
   index: 'index.html'
 }));
 
-app.listen(config.port);
-
-console.log(`Listening on ${config.host}:${config.port}`);
+app.listen(config.port, console.log(`Listening on ${config.host}:${config.port}`));
---
+++ static/assets/js/index.js
@@ -0,0 +1,20 @@
+let app = null;
+// Handle pops
+const handlePop = (evt) => {
+    let path = evt.target.location.pathname;
+    if (path === '/') app.newDocument(true);
+    else app.loadDocument(path.substring(1, path.length));
+};
+// Set up the pop state to handle loads, skipping the first load
+// to make chrome behave like others:
+// http://code.google.com/p/chromium/issues/detail?id=63040
+setTimeout(() => {
+    window.onpopstate = (evt) => {
+        try { handlePop(evt); } catch(err) { /* not loaded yet */ }
+    };
+}, 1000);
+// Construct app and load initial path
+$(() => {
+    app = new haste('hastebin', { twitter: true });
+    handlePop({ target: window });
+});
